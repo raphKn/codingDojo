780074f7634984cf4e4addc87268e26bba8d84da
Raphael Knecht <raphael@funk.net>
2018-11-01 18:03:09 +0100

    prepare composite pattern for parallelerTrainingsbetrieb

---
+++ src/test/verein/mannschaft/ParalellerTrainingsbetriebTest.java
@@ -0,0 +1,39 @@
+package verein.mannschaft;
+
+public class ParalellerTrainingsbetriebTest {
+
+/*    @Test
+    public void JugendWithFreizeit() {
+        Jugendmannschaft jugendmannschaft = new JugendMannschaftTestdataBuilder().build();
+        Freizeitmannschaft freizeitmannschaft = new FreizeitMannschaftTestdataBuilder().build();
+
+        GemeinsameTrainierende gemeinsameTrainierende = new GemeinsameTrainierende();
+
+        gemeinsameTrainierende.addMannschaft(jugendmannschaft);
+        gemeinsameTrainierende.addMannschaft(freizeitmannschaft);
+
+        Trainingsplan trainingsplan = gemeinsameTrainierende.getTrainingsplan(new Doppelpass());
+
+        assertThat(trainingsplan.toString(),
+            is("Trainingsplan{materialListe=[Material{bezeichnung='Ball', menge=7}, Material{bezeichnung='Stangen', menge=5}], dauerInMinuten=25}"));
+    }
+
+    @Test
+    public void JugendWithFreizeitWarmlaufenDoppelpass() {
+        Jugendmannschaft jugendmannschaft = new JugendMannschaftTestdataBuilder().build();
+        Freizeitmannschaft freizeitmannschaft = new FreizeitMannschaftTestdataBuilder().build();
+
+        GemeinsameTrainierende paralellerTrainingsbetrieb = new GemeinsameTrainierende();
+        paralellerTrainingsbetrieb.addMannschaft(jugendmannschaft);
+        paralellerTrainingsbetrieb.addMannschaft(freizeitmannschaft);
+
+        SerielleUebungen trainingsplaene = new SerielleUebungen();
+        trainingsplaene.addUebung(new Doppelpass());
+        trainingsplaene.addUebung(new Stabilisation());
+
+        Trainingsplan accept = paralellerTrainingsbetrieb.getTrainingsplan(trainingsplaene);
+
+        assertThat(accept.toString(),
+            is("Trainingsplan{materialListe=[Material{bezeichnung='Ball', menge=17}, Material{bezeichnung='Stangen', menge=5}], dauerInMinuten=39}"));
+    }*/
+}
